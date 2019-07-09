ce204ebf3130b8d615501a5112205da4a6be5589
ichrak <ichrak.smati@esprit.tn>
2019-07-09 11:05:24 +0100

    router

---
+++ src/app/front-page/emploi/emploi.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'emploi',
+  templateUrl: './emploi.component.html'
+
+
+})
+export class EmploiComponent {
+  title = 'recrutement';
+}
---
+++ src/app/front-page/recherche/recherche.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'recherche',
+  templateUrl: './recherche.component.html'
+
+
+})
+export class RechercheComponent {
+  title = 'recrutement';
+}
